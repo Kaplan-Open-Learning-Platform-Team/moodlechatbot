ed0f5b3a35633405f0f89734dcc491b8b5e333e0
Anrich Potgieter <anrich.potgieter@kaplan.com>
2024-08-29 09:51:14 +0100

    comment out all of the event API calls

--- view.php
+++ view.php
@@ -48,10 +48,10 @@ $modulecontext = context_module::instance($cm->id);
 #$event = \mod_moodlechatbot\event\course_module_viewed::create(array(
 #    'objectid' => $moduleinstance->id,
 #    'context' => $modulecontext
-#));
-$event->add_record_snapshot('course', $course);
-$event->add_record_snapshot('moodlechatbot', $moduleinstance);
-$event->trigger();
+##));
+#$event->add_record_snapshot('course', $course);
+#$event->add_record_snapshot('moodlechatbot', $moduleinstance);
+#$event->trigger();
 
 $PAGE->set_url('/mod/moodlechatbot/view.php', array('id' => $cm->id));
 $PAGE->set_title(format_string($moduleinstance->name));
