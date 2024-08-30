d43abf1e8b29e0116c2037d7309f7a8d38724765
Anrich Potgieter <anrich.potgieter@kaplan.com>
2024-08-30 12:13:53 +0100

    add additional settings

--- settings.php
+++ settings.php
@@ -19,17 +19,42 @@
  *
  * @package     mod_moodlechatbot
  * @category    admin
- * @copyright   2024 Kaplan Open Learning <kol-learning-tech@kaplan.com>
+ * @copyright   2024 Your Name <your@email.com>
  * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
 
-if ($hassiteconfig) {
-    $settings = new admin_settingpage('mod_moodlechatbot_settings', new lang_string('pluginname', 'mod_moodlechatbot'));
+if ($ADMIN->fulltree) {
+    
+    // Add a setting for the Groq API key.
+    $settings->add(new admin_setting_configtext(
+        'mod_moodlechatbot/groq_api_key',
+        get_string('groq_api_key', 'mod_moodlechatbot'),
+        get_string('groq_api_key_desc', 'mod_moodlechatbot'),
+        '',  // default value
+        PARAM_TEXT
+    ));
 
-    // phpcs:ignore Generic.CodeAnalysis.EmptyStatement.DetectedIf
-    if ($ADMIN->fulltree) {
-        // TODO: Define actual plugin settings page and add it to the tree - {@link https://docs.moodle.org/dev/Admin_settings}.
-    }
+    // Add a setting for the AI model to use.
+    $settings->add(new admin_setting_configselect(
+        'mod_moodlechatbot/ai_model',
+        get_string('ai_model', 'mod_moodlechatbot'),
+        get_string('ai_model_desc', 'mod_moodlechatbot'),
+        'mixtral-8x7b-32768',  // default value
+        array(
+            'mixtral-8x7b-32768' => 'Mixtral 8x7B',
+            'llama2-70b-4096' => 'LLaMA2 70B',
+            // Add more models as needed
+        )
+    ));
+
+    // Add a setting for maximum response tokens.
+    $settings->add(new admin_setting_configtext(
+        'mod_moodlechatbot/max_tokens',
+        get_string('max_tokens', 'mod_moodlechatbot'),
+        get_string('max_tokens_desc', 'mod_moodlechatbot'),
+        '1000',  // default value
+        PARAM_INT
+    ));
 }
