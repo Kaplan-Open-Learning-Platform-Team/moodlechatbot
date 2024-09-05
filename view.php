d70fcdd801a92c97ea3abf76cb5838403072d5a0
Anrich Potgieter <anrich.potgieter@kaplan.com>
2024-09-05 21:44:55 +0100

    update view

--- view.php
+++ view.php
@@ -25,32 +25,25 @@
 require(__DIR__.'/../../config.php');
 require_once(__DIR__.'/lib.php');
 
+// Course module id.
 $id = optional_param('id', 0, PARAM_INT);
-$m  = optional_param('m', 0, PARAM_INT);
+
+// Activity instance id.
+$m = optional_param('m', 0, PARAM_INT);
 
 if ($id) {
-    $cm             = get_coursemodule_from_id('moodlechatbot', $id, 0, false, MUST_EXIST);
-    $course         = $DB->get_record('course', array('id' => $cm->course), '*', MUST_EXIST);
+    $cm = get_coursemodule_from_id('moodlechatbot', $id, 0, false, MUST_EXIST);
+    $course = $DB->get_record('course', array('id' => $cm->course), '*', MUST_EXIST);
     $moduleinstance = $DB->get_record('moodlechatbot', array('id' => $cm->instance), '*', MUST_EXIST);
-} else if ($m) {
-    $moduleinstance = $DB->get_record('moodlechatbot', array('id' => $m), '*', MUST_EXIST);
-    $course         = $DB->get_record('course', array('id' => $moduleinstance->course), '*', MUST_EXIST);
-    $cm             = get_coursemodule_from_instance('moodlechatbot', $moduleinstance->id, $course->id, false, MUST_EXIST);
 } else {
-    print_error(get_string('missingidandcmid', 'mod_moodlechatbot'));
+    $moduleinstance = $DB->get_record('moodlechatbot', array('id' => $m), '*', MUST_EXIST);
+    $course = $DB->get_record('course', array('id' => $moduleinstance->course), '*', MUST_EXIST);
+    $cm = get_coursemodule_from_instance('moodlechatbot', $moduleinstance->id, $course->id, false, MUST_EXIST);
 }
 
 require_login($course, true, $cm);
 
 $modulecontext = context_module::instance($cm->id);
-
-$event = \mod_moodlechatbot\event\course_module_viewed::create(array(
-    'objectid' => $moduleinstance->id,
-    'context' => $modulecontext
-));
-$event->add_record_snapshot('course', $course);
-$event->add_record_snapshot('moodlechatbot', $moduleinstance);
-$event->trigger();
 
 $PAGE->set_url('/mod/moodlechatbot/view.php', array('id' => $cm->id));
 $PAGE->set_title(format_string($moduleinstance->name));
@@ -59,23 +52,18 @@ $PAGE->set_context($modulecontext);
 
 echo $OUTPUT->header();
 
-echo $OUTPUT->heading(format_string($moduleinstance->name), 2);
+echo $OUTPUT->heading(format_string($moduleinstance->name));
 
-if ($moduleinstance->intro) {
-    echo $OUTPUT->box(format_module_intro('moodlechatbot', $moduleinstance, $cm->id), 'generalbox mod_introbox', 'moodlechatbotintro');
-}
+// Display a simple chat interface.
+echo html_writer::start_div('moodlechatbot-container');
+echo html_writer::div('', 'moodlechatbot-messages', array('id' => 'moodlechatbot-messages'));
+echo html_writer::start_div('moodlechatbot-input');
+echo html_writer::tag('textarea', '', array('id' => 'moodlechatbot-input', 'placeholder' => get_string('typemessage', 'mod_moodlechatbot')));
+echo html_writer::tag('button', get_string('send', 'mod_moodlechatbot'), array('id' => 'moodlechatbot-send'));
+echo html_writer::end_div();
+echo html_writer::end_div();
 
-$chatid = 'moodlechatbot-' . uniqid();
-
-$templatecontext = [
-    'chatid' => $chatid,
-    'chatbotid' => $cm->instance,
-    'placeholder' => get_string('typemessage', 'mod_moodlechatbot'),
-    'sendlabel' => get_string('send', 'mod_moodlechatbot'),
-];
-
-echo $OUTPUT->render_from_template('mod_moodlechatbot/chat_interface', $templatecontext);
-
-$PAGE->requires->js_call_amd('mod_moodlechatbot/chat', 'init', [$chatid]);
+// Include JavaScript for chat functionality.
+$PAGE->requires->js_call_amd('mod_moodlechatbot/chat', 'init');
 
 echo $OUTPUT->footer();
