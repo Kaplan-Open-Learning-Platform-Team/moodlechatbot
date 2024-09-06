36dabfa8f3edf83dfe2a4fa99e739344aeb2bb74
cordockj <james.cordock@kaplan.com>
2024-09-06 15:41:09 +0100

    Create services.php defined the ajax endpoint

---
+++ db/services.php
@@ -0,0 +1,11 @@
+// db/services.php
+$functions = array(
+    'mod_moodlechatbot_get_enrolled_courses' => array(
+        'classname'   => 'mod_moodlechatbot_external',
+        'methodname'  => 'get_enrolled_courses',
+        'classpath'   => 'mod/moodlechatbot/externallib.php',
+        'description' => 'Returns a list of courses the current user is enrolled in.',
+        'type'        => 'read',
+        'ajax'        => true,
+    ),
+);
