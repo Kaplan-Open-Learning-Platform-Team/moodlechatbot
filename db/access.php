712562d92020d3d831e6abba554994e763760313
cordockj <james.cordock@kaplan.com>
2024-09-06 23:16:13 +0100

    Update access.php

--- db/access.php
+++ db/access.php
@@ -1,11 +1,14 @@
 // db/access.php
+<?php
+defined('MOODLE_INTERNAL') || die();
+
 $capabilities = array(
     'mod/moodlechatbot:view' => array(
         'captype' => 'read',
-        'contextlevel' => CONTEXT_MODULE,
+        'contextlevel' => CONTEXT_SYSTEM, // Use CONTEXT_SYSTEM for global access
         'archetypes' => array(
-            'student' => CAP_ALLOW,
-            'teacher' => CAP_ALLOW,
+            'student' => CAP_ALLOW,  // Grant access to students
+            'teacher' => CAP_ALLOW,  // Grant access to teachers
         ),
     ),
 );
