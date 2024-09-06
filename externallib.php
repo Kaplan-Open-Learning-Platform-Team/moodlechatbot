7b773d7fed1790db7894af7f906d5ed253ae6e8d
cordockj <james.cordock@kaplan.com>
2024-09-06 21:10:25 +0100

    Create externallib.php added get_enrolled_courses()

---
+++ externallib.php
@@ -0,0 +1,24 @@
+// externallib.php
+defined('MOODLE_INTERNAL') || die();
+
+require_once("$CFG->libdir/externallib.php");
+
+class mod_moodlechatbot_external extends external_api {
+    public static function get_enrolled_courses_parameters() {
+        return new external_function_parameters([]);
+    }
+
+    public static function get_enrolled_courses() {
+        global $USER;
+        return mod_moodlechatbot_get_enrolled_courses($USER->id);
+    }
+
+    public static function get_enrolled_courses_returns() {
+        return new external_multiple_structure(
+            new external_single_structure([
+                'id' => new external_value(PARAM_INT, 'Course ID'),
+                'fullname' => new external_value(PARAM_TEXT, 'Course full name'),
+            ])
+        );
+    }
+}
