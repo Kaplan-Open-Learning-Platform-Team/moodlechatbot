b784161508544469992349a598aab3f6b8f1eda9
cordockj <james.cordock@kaplan.com>
2024-09-06 15:27:09 +0100

    Update ajax.php added code to handle the AJAX request and return the user's enrolled courses.

--- ajax.php
+++ ajax.php
@@ -1,1 +1,15 @@
+// ajax.php
+require_once('../../config.php');
+require_once('lib.php');
 
+// Require the user to be logged in.
+require_login();
+
+// Get the current user's id
+$userid = $USER->id;
+
+// Get enrolled courses for the user
+$courses = mod_moodlechatbot_get_enrolled_courses($userid);
+
+// Return the list of courses as JSON
+echo json_encode($courses);
