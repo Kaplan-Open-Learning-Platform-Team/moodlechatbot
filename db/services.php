5529f2390a6bef700d6663d62be686f683d40b89
cordockj <james.cordock@kaplan.com>
2024-09-07 12:37:43 +0100

    Update services.php

--- db/services.php
+++ db/services.php
@@ -1,11 +1,22 @@
-// db/services.php
+<?php
+
 $functions = array(
     'mod_moodlechatbot_get_enrolled_courses' => array(
         'classname'   => 'mod_moodlechatbot_external',
         'methodname'  => 'get_enrolled_courses',
         'classpath'   => 'mod/moodlechatbot/externallib.php',
-        'description' => 'Returns a list of courses the current user is enrolled in.',
+        'description' => 'Returns a list of courses the user is enrolled in',
         'type'        => 'read',
-        'ajax'        => true,
+        'ajax'        => true, // Enable AJAX
+        'loginrequired' => true,
+        'capabilities' => ''
+    ),
+);
+
+$services = array(
+    'Moodle Chatbot Service' => array(
+        'functions' => array('mod_moodlechatbot_get_enrolled_courses'),
+        'restrictedusers' => 0,
+        'enabled' => 1,
     ),
 );
