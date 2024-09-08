bf9f1fa2c1fcd823720ed04cd2ba428c4389b6aa
cordockj <james.cordock@kaplan.com>
2024-09-08 22:32:16 +0100

    Update view.php added get_user_courses function

--- view.php
+++ view.php
@@ -24,12 +24,45 @@
 
 require(__DIR__.'/../../config.php');
 require_once(__DIR__.'/lib.php');
+require_once($CFG->dirroot.'/enrol/locallib.php');  // ADD THIS LINE
 
 // Course module id.
 $id = optional_param('id', 0, PARAM_INT);
 
 // Activity instance id.
 $m = optional_param('m', 0, PARAM_INT);
+
+// Check if this is an AJAX request to retrieve courses.
+if (optional_param('ajax', false, PARAM_BOOL)) {
+    global $USER, $DB;
+
+    // Function to retrieve the user's enrolled courses.
+    function get_user_courses($userid) {
+        global $DB;
+        $courses = enrol_get_users_courses($userid);
+        if ($courses) {
+            foreach ($courses as &$course) {
+                $course->fullname = $DB->get_field('course', 'fullname', array('id' => $course->id));
+            }
+        }
+        return $courses;
+    }
+
+    // Ensure the user is logged in.
+    require_login();
+
+    // Get the user's enrolled courses.
+    $courses = get_user_courses($USER->id);
+
+    // Return the courses as a JSON response.
+    header('Content-Type: application/json');
+    if ($courses) {
+        echo json_encode($courses);
+    } else {
+        echo json_encode(array('error' => 'No courses found.'));
+    }
+    exit();
+}
 
 if ($id) {
     $cm = get_coursemodule_from_id('moodlechatbot', $id, 0, false, MUST_EXIST);
@@ -44,14 +77,6 @@ if ($id) {
 require_login($course, true, $cm);
 
 $modulecontext = context_module::instance($cm->id);
-
-#$event = \mod_moodlechatbot\event\course_module_viewed::create(array(
-#    'objectid' => $moduleinstance->id,
-#    'context' => $modulecontext
-##));
-#$event->add_record_snapshot('course', $course);
-#$event->add_record_snapshot('moodlechatbot', $moduleinstance);
-#$event->trigger();
 
 $PAGE->set_url('/mod/moodlechatbot/view.php', array('id' => $cm->id));
 $PAGE->set_title(format_string($moduleinstance->name));
@@ -73,25 +98,5 @@ $userid = $USER->id;  // Capture the user ID.
 
 // Include the JavaScript module and Pass the user ID to your JavaScript file.
 $PAGE->requires->js_call_amd('mod_moodlechatbot/interface', 'init', array($userid));
-
-// Check if this is an AJAX request to get user courses.
-if (optional_param('action', '', PARAM_ALPHA) === 'get_courses') {
-    global $USER;
-
-    // Call your existing function to get the user's courses.
-    $courses = get_user_courses($USER->id);
-
-    // Prepare the course names to return.
-    $course_names = [];
-    if ($courses) {
-        foreach ($courses as $course) {
-            $course_names[] = $course->fullname;
-        }
-    }
-
-    // Return the course list as JSON.
-    echo json_encode(['courses' => $course_names]);
-    exit;
-}
 
 echo $OUTPUT->footer();
