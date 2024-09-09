ee83bdc73adb8dd4c1dbe0863770e89dfb62c15f
cordockj <james.cordock@kaplan.com>
2024-09-09 23:02:35 +0100

    Update view.php

--- view.php
+++ view.php
@@ -2,17 +2,19 @@
 //view.php
 require(__DIR__.'/../../config.php');
 require_once(__DIR__.'/lib.php');
-require_once($CFG->dirroot.'/enrol/locallib.php'); 
+require_once($CFG->dirroot.'/enrol/locallib.php');
 
 // Course module id.
 $id = optional_param('id', 0, PARAM_INT);
-
 // Activity instance id.
 $m = optional_param('m', 0, PARAM_INT);
 
-// Check if this is an AJAX request to retrieve courses.
-if (optional_param('ajax', false, PARAM_BOOL)) {
+// AJAX request handling for course enrollment
+function handle_ajax_request() {
     global $USER, $DB;
+    
+    // Ensure the user is logged in.
+    require_login();
 
     // Function to retrieve the user's enrolled courses.
     function get_user_courses($userid) {
@@ -20,74 +22,68 @@ if (optional_param('ajax', false, PARAM_BOOL)) {
         $courses = enrol_get_users_courses($userid);
         if ($courses) {
             foreach ($courses as &$course) {
-                $course->fullname = $DB->get_field('course', 'fullname', array('id' => $course->id));
+                $course->fullname = $DB->get_field('course', 'fullname', ['id' => $course->id]);
             }
         }
         return $courses;
     }
 
-    // Ensure the user is logged in.
-    require_login();
-
-    // Get the module context for capability check.
+    // Check for course module or instance
+    $id = optional_param('id', 0, PARAM_INT);
+    $m = optional_param('m', 0, PARAM_INT);
+    
     if ($id) {
         $cm = get_coursemodule_from_id('moodlechatbot', $id, 0, false, MUST_EXIST);
         $modulecontext = context_module::instance($cm->id);
-    } else if ($m) {
-        $moduleinstance = $DB->get_record('moodlechatbot', array('id' => $m), '*', MUST_EXIST);
+    } elseif ($m) {
+        $moduleinstance = $DB->get_record('moodlechatbot', ['id' => $m], '', MUST_EXIST);
         $cm = get_coursemodule_from_instance('moodlechatbot', $moduleinstance->id, $moduleinstance->course, false, MUST_EXIST);
         $modulecontext = context_module::instance($cm->id);
     } else {
-        // Handle the case where neither $id nor $m are provided.
-        // You might want to return an error or redirect the user.
         die("Invalid module or instance ID."); 
     }
-
+    
     // Perform the capability check.
     require_capability('mod/moodlechatbot:viewuserenrollments', $modulecontext);
 
-    // Get the user's enrolled courses.
+    // Get user's enrolled courses.
     $courses = get_user_courses($USER->id);
-
+    
     // Return the course names as a JSON response.
     header('Content-Type: application/json');
-    if ($courses) {
-        $courseNames = array_map(function($course) { 
-            return $course->fullname; 
-        }, $courses);
-        echo json_encode(['courses' => $courseNames]);
-    } else {
-        echo json_encode(['courses' => []]); // Return an empty array if no courses found.
-    }
+    $courseNames = $courses ? array_map(function($course) { return $course->fullname; }, $courses) : [];
+    echo json_encode(['courses' => $courseNames]);
     exit();
 }
 
+if (optional_param('ajax', false, PARAM_BOOL)) {
+    handle_ajax_request();
+}
+
+// Regular non-AJAX view logic...
 if ($id) {
     $cm = get_coursemodule_from_id('moodlechatbot', $id, 0, false, MUST_EXIST);
-    $course = $DB->get_record('course', array('id' => $cm->course), '*', MUST_EXIST);
-    $moduleinstance = $DB->get_record('moodlechatbot', array('id' => $cm->instance), '*', MUST_EXIST);
+    $course = $DB->get_record('course', ['id' => $cm->course], '', MUST_EXIST);
+    $moduleinstance = $DB->get_record('moodlechatbot', ['id' => $cm->instance], '', MUST_EXIST);
 } else {
-    $moduleinstance = $DB->get_record('moodlechatbot', array('id' => $m), '*', MUST_EXIST);
-    $course = $DB->get_record('course', array('id' => $moduleinstance->course), '*', MUST_EXIST);
+    $moduleinstance = $DB->get_record('moodlechatbot', ['id' => $m], '', MUST_EXIST);
+    $course = $DB->get_record('course', ['id' => $moduleinstance->course], '*', MUST_EXIST);
     $cm = get_coursemodule_from_instance('moodlechatbot', $moduleinstance->id, $course->id, false, MUST_EXIST);
 }
 
 require_login($course, true, $cm);
-
 $modulecontext = context_module::instance($cm->id);
-
-$PAGE->set_url('/mod/moodlechatbot/view.php', array('id' => $cm->id));
+$PAGE->set_url('/mod/moodlechatbot/view.php', ['id' => $cm->id]);
 $PAGE->set_title(format_string($moduleinstance->name));
 $PAGE->set_heading(format_string($course->fullname));
 $PAGE->set_context($modulecontext);
 
 echo $OUTPUT->header();
-
-echo html_writer::start_tag('div', array('id' => 'moodlechatbot-container'));
-echo html_writer::tag('div', '', array('id' => 'moodlechatbot-messages'));
-echo html_writer::start_tag('div', array('id' => 'moodlechatbot-input'));
-echo html_writer::tag('textarea', '', array('id' => 'moodlechatbot-textarea', 'placeholder' => 'Type your message here...'));
-echo html_writer::tag('button', 'Send', array('id' => 'moodlechatbot-send'));
+echo html_writer::start_tag('div', ['id' => 'moodlechatbot-container']);
+echo html_writer::tag('div', '', ['id' => 'moodlechatbot-messages']);
+echo html_writer::start_tag('div', ['id' => 'moodlechatbot-input']);
+echo html_writer::tag('textarea', '', ['id' => 'moodlechatbot-textarea', 'placeholder' => 'Type your message here...']);
+echo html_writer::tag('button', 'Send', ['id' => 'moodlechatbot-send']);
 echo html_writer::end_tag('div');
 echo html_writer::end_tag('div');
 
@@ -95,7 +91,6 @@ global $USER;
 $userid = $USER->id;
 
 // Include the JavaScript module and pass the user ID.
-$PAGE->requires->js_call_amd('mod_moodlechatbot/interface', 'init', array($userid));
-
+$PAGE->requires->js_call_amd('mod_moodlechatbot/interface', 'init', [$userid]);
 echo $OUTPUT->footer();
 ?>
