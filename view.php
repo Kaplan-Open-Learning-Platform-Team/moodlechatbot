e5174372a63bba0d22abb6bf3a9a265c40bdcada
cordockj <james.cordock@kaplan.com>
2024-09-09 22:25:11 +0100

    Update view.php

--- view.php
+++ view.php
@@ -1,27 +1,5 @@
 <?php
-// This file is part of Moodle - https://moodle.org/
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
-
-/**
- * Prints an instance of mod_moodlechatbot.
- *
- * @package     mod_moodlechatbot
- * @copyright   2024 Kaplan Open Learning <kol-learning-tech@kaplan.com>
- * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
-
+//view.php
 require(__DIR__.'/../../config.php');
 require_once(__DIR__.'/lib.php');
 require_once($CFG->dirroot.'/enrol/locallib.php'); 
