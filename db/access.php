790e6b07f83876b7ce684e831c3547495f808b71
cordockj <james.cordock@kaplan.com>
2024-09-09 17:36:02 +0100

    Update access.php

--- db/access.php
+++ db/access.php
@@ -1,7 +1,14 @@
-// In your plugin's db/access.php file
+<?php
+
+// This file defines the capabilities for the Moodle Chatbot plugin.
+
+defined('MOODLE_INTERNAL') || die();
+
 $capabilities = array(
-    'local/yourchatbotplugin:viewuserenrollments' => array(
-        'riskbitmask' => RISK_PERSONAL,
+
+    'mod/moodlechatbot:viewuserenrollments' => array( // Updated component name
+
+        'riskbitmask' => RISK_PERSONAL, 
         'captype' => 'read',
         'contextlevel' => CONTEXT_COURSE,
         'archetypes' => array(
@@ -11,4 +18,5 @@ $capabilities = array(
         )
     )
 );
-];
+
+?>
