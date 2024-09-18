d8da887abf539bf1f01d4691f14fc87b4d40a0b8
cordockj <james.cordock@kaplan.com>
2024-09-18 16:22:01 +0100

    Create enrolled_courses.js

---
+++ amd/src/enrolled_courses.js
@@ -0,0 +1,22 @@
+define(['core/ajax', 'core/str'], function(Ajax, Str) {
+    return {
+        getCourses: function() {
+            var request = Ajax.call([{
+                methodname: 'yourpluginname_get_enrolled_courses',  // Call the web service.
+                args: {}
+            }]);
+
+            request[0].done(function(response) {
+                var courses = response;
+                var message = 'You are enrolled in the following courses:\n';
+                courses.forEach(function(course) {
+                    message += '- ' + course.fullname + '\n';
+                });
+                console.log(message);
+                // The chatbot can then display this message in the interface.
+            }).fail(function(error) {
+                console.error("Error fetching enrolled courses: ", error);
+            });
+        }
+    };
+});
