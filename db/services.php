997814df5fbf473a19bc8b497e344c4d0ca667f7
cordockj <james.cordock@kaplan.com>
2024-09-18 17:24:46 +0100

    Update services.php

--- db/services.php
+++ db/services.php
@@ -1,8 +1,8 @@
 $functions = [
-    'yourpluginname_get_enrolled_courses' => [
-        'classname' => 'yourpluginname\external\get_enrolled_courses',
+    'moodlechatbot_get_enrolled_courses' => [
+        'classname' => 'moodlechatbot\external\get_enrolled_courses',
         'methodname' => 'get_enrolled_courses',
-        'classpath' => 'yourpluginname/classes/external/get_enrolled_courses.php',
+        'classpath' => 'moodlechatbot/classes/external/get_enrolled_courses.php',
         'description' => 'Returns the list of courses the user is enrolled in',
         'type' => 'read',
         'ajax' => true, // This makes it available for AJAX calls
