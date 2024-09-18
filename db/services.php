69ca2f57622301f48e2cb8f583a04943ba2fa516
cordockj <james.cordock@kaplan.com>
2024-09-18 19:59:06 +0100

    Update services.php

--- db/services.php
+++ db/services.php
@@ -1,12 +1,45 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Moodle Chatbot external functions and service definitions.
+ *
+ * @package    mod_moodlechatbot
+ * @category   webservice
+ * @copyright  2024 Your Name <your@email.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
 $functions = [
-    'moodlechatbot_get_enrolled_courses' => [
-        'classname' => 'moodlechatbot\external\get_enrolled_courses',
-        'methodname' => 'get_enrolled_courses',
-        'classpath' => 'moodlechatbot/classes/external/get_enrolled_courses.php',
-        'description' => 'Returns the list of courses the user is enrolled in',
-        'type' => 'read',
-        'ajax' => true, // This makes it available for AJAX calls
-        'capabilities' => '',
+    'mod_moodlechatbot_get_enrolled_courses' => [
+        'classname'   => 'mod_moodlechatbot_external',
+        'methodname'  => 'get_enrolled_courses',
+        'classpath'   => 'mod/moodlechatbot/classes/external.php',
+        'description' => 'Get enrolled courses for the current user',
+        'type'        => 'read',
+        'ajax'        => true,
+    ],
+];
+
+$services = [
+    'Moodle Chatbot Service' => [
+        'functions' => ['mod_moodlechatbot_get_enrolled_courses'],
+        'restrictedusers' => 0,
+        'enabled' => 1,
     ],
 ];
