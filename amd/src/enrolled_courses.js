bd9e0e6929fbf0c5b20359ede2bb7c222276b6cb
cordockj <james.cordock@kaplan.com>
2024-09-18 17:36:25 +0100

    Update enrolled_courses.js

--- amd/src/enrolled_courses.js
+++ amd/src/enrolled_courses.js
@@ -1,17 +1,14 @@
 // File: amd/src/enrolled_courses.js
-define(['core/ajax'], function(ajax) {
-    return {
-        getCourses: function() {
-            return ajax.call([{
-                methodname: 'moodlechatbot_get_enrolled_courses',
-                args: {}
-            }])[0].then(response => {
-                return response.courses || [];
-            }).catch(error => {
-                console.error('Error fetching enrolled courses:', error);
-                throw error;
-            });
-        }
-    };
-});
+define(['core/ajax'], (ajax) => ({ // ES6 arrow function and object literal return
 
+    getCourses: () => 
+        ajax.call([{
+            methodname: 'moodlechatbot_get_enrolled_courses',
+            args: {}
+        }])[0].then(response => response.courses || []) 
+        .catch(error => {
+            console.error('Error fetching enrolled courses:', error); // Consider using a Moodle logger here
+            throw error;
+        })
+
+}));
