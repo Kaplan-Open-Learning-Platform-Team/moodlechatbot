b06e1d62e1b3ae342787ff287b0bd856b687b306
Anrich Potgieter <anrich.potgieter@kaplan.com>
2024-09-20 12:39:19 +0100

    attempting to fix permission issue

--- db/access.php
+++ db/access.php
@@ -43,42 +43,41 @@
  * @copyright  2024 Kaplan Open Learning <kol-learning-tech@kaplan.com>
  * @license    https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-
 defined('MOODLE_INTERNAL') || die();
 
 $capabilities = [
-    'mod/moodlechatbot:addinstance' => [
-        'riskbitmask' => RISK_XSS,
-        'captype' => 'write',
-        'contextlevel' => CONTEXT_COURSE,
-        'archetypes' => [
-            'editingteacher' => CAP_ALLOW,
-            'manager' => CAP_ALLOW
-        ],
-        'clonepermissionsfrom' => 'moodle/course:manageactivities'
+  'mod/moodlechatbot:addinstance' => [
+    'riskbitmask' => RISK_XSS,
+    'captype' => 'write',
+    'contextlevel' => CONTEXT_COURSE,
+    'archetypes' => [
+      'editingteacher' => CAP_ALLOW,
+      'manager' => CAP_ALLOW
     ],
+    'clonepermissionsfrom' => 'moodle/course:manageactivities'
+  ],
 
-    'mod/moodlechatbot:view' => [
-        'captype' => 'read',
-        'contextlevel' => CONTEXT_MODULE,
-        'archetypes' => [
-            'guest' => CAP_ALLOW,
-            'student' => CAP_ALLOW,
-            'teacher' => CAP_ALLOW,
-            'editingteacher' => CAP_ALLOW,
-            'manager' => CAP_ALLOW
-        ]
-    ],
+  'mod/moodlechatbot:view' => [
+    'captype' => 'read',
+    'contextlevel' => CONTEXT_MODULE,
+    'archetypes' => [
+      'guest' => CAP_ALLOW,
+      'student' => CAP_ALLOW,
+      'teacher' => CAP_ALLOW,
+      'editingteacher' => CAP_ALLOW,
+      'manager' => CAP_ALLOW
+    ]
+  ],
 
-    'mod/moodlechatbot:use' => [
-        'riskbitmask' => RISK_SPAM,
-        'captype' => 'write',
-        'contextlevel' => CONTEXT_MODULE,
-        'archetypes' => [
-            'student' => CAP_ALLOW,
-            'teacher' => CAP_ALLOW,
-            'editingteacher' => CAP_ALLOW,
-            'manager' => CAP_ALLOW
-        ]
-    ],
+  'mod/moodlechatbot:use' => [
+    'riskbitmask' => RISK_SPAM,
+    'captype' => 'write',
+    'contextlevel' => CONTEXT_MODULE,
+    'archetypes' => [
+      'student' => CAP_ALLOW,
+      'teacher' => CAP_ALLOW,
+      'editingteacher' => CAP_ALLOW,
+      'manager' => CAP_ALLOW
+    ]
+  ],
 ];
