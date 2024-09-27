0a283240f13ed177c2e48805be927152dd80b8de
cordockj <james.cordock@kaplan.com>
2024-09-27 16:27:01 +0100

    Update send_message.php

--- classes/external/send_message.php
+++ classes/external/send_message.php
@@ -1,5 +1,18 @@
 <?php
-// classes/external/send_message.php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 namespace mod_moodlechatbot\external;
 
@@ -10,8 +23,10 @@ require_once($CFG->libdir . '/externallib.php');
 use external_api;
 use external_function_parameters;
 use external_value;
+use external_single_structure;
 
 class send_message extends external_api {
+
     /**
      * Returns description of method parameters
      * @return external_function_parameters
@@ -43,7 +58,7 @@ class send_message extends external_api {
 
     /**
      * Returns description of method result value
-     * @return external_description
+     * @return external_single_structure
      */
     public static function execute_returns() {
         return new external_single_structure([
