359545cd3ee1c1cbcb1bc81990e25a84179f0a06
cordockj <james.cordock@kaplan.com>
2024-09-28 11:11:27 +0100

    Update tool_manager.php

--- classes/tool_manager.php
+++ classes/tool_manager.php
@@ -27,38 +27,3 @@ class tool_manager {
         return null;
     }
 }
-
-// classes/tool.php
-
-namespace mod_moodlechatbot;
-
-defined('MOODLE_INTERNAL') || die();
-
-abstract class tool {
-    abstract public function execute($params = []);
-}
-
-// classes/tools/get_enrolled_courses.php
-
-namespace mod_moodlechatbot\tools;
-
-defined('MOODLE_INTERNAL') || die();
-
-class get_enrolled_courses extends \mod_moodlechatbot\tool {
-    public function execute($params = []) {
-        global $USER, $DB;
-
-        $courses = enrol_get_users_courses($USER->id, true, 'id, shortname, fullname');
-        $result = [];
-
-        foreach ($courses as $course) {
-            $result[] = [
-                'id' => $course->id,
-                'shortname' => $course->shortname,
-                'fullname' => $course->fullname
-            ];
-        }
-
-        return $result;
-    }
-}
