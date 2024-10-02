d181ac228e7cd6cc16e831afa0115f288887e003
cordockj <james.cordock@kaplan.com>
2024-10-02 12:36:22 +0100

    claude changes 1b; trying to fix tool call

--- classes/tools/get_enrolled_courses.php
+++ classes/tools/get_enrolled_courses.php
@@ -1,18 +1,43 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Get enrolled courses tool for the Moodle Chatbot plugin.
+ *
+ * @package    mod_moodlechatbot
+ * @copyright  2024 Your Name &lt;your@email.com&gt;
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 
 namespace mod_moodlechatbot\tools;
 
 defined('MOODLE_INTERNAL') || die();
 
+require_once(__DIR__ . '/../tool.php');
+
 class get_enrolled_courses extends \mod_moodlechatbot\tool {
     public function execute($params = []) {
-        global $DB;
+        global $USER, $DB;
 
         if (empty($params['userid'])) {
-            return ['error' => 'User ID is required'];
+            $userid = $USER->id;
+        } else {
+            $userid = $params['userid'];
         }
 
-        $userid = $params['userid'];
         $courses = enrol_get_users_courses($userid, true, 'id, shortname, fullname');
         $result = [];
 
