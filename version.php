71d7074a47e96c500e3d38d1ed7045783df2de9d
cordockj <james.cordock@kaplan.com>
2024-10-02 22:48:37 +0100

    Update version.php

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_moodlechatbot';
 $plugin->release = '0.1.1';
-$plugin->version = 20241002001;
+$plugin->version = 202410020001;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_ALPHA;
