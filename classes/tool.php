08cf9fe124aa7be666fa908a4f5779156a8e0e0c
cordockj <james.cordock@kaplan.com>
2024-10-12 15:05:29 +0100

    fixing debugging error

--- classes/tool.php
+++ classes/tool.php
@@ -8,12 +8,10 @@ defined('MOODLE_INTERNAL') || die();
 abstract class tool {
 
     public function __construct() {
-        // This helps verify the correct tool class is loaded.  It is minimal to avoid excessive logging.
+        // This helps verify the correct tool class is loaded. It is minimal to avoid excessive logging.
         $className = get_class($this);  // Get the name of the instantiated concrete class.
-        debug_to_console("Tool class instantiated: " . $className); 
+        debugging("Tool class instantiated: " . $className, DEBUG_DEVELOPER);
     }
-
 
     abstract public function execute($params = []);
 }
-?>
