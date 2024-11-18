7cf44fb9aa098aec9f602bb465b39cb30e6b4e5b
cordockj <james.cordock@kaplan.com>
2024-11-18 17:28:15 +0000

    incremental

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_moodlechatbot';
 $plugin->release = '0.1.1';
-$plugin->version = 20243009003;
+$plugin->version = 20243009004;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_ALPHA;
